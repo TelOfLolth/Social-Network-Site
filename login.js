718b59b450bfbdaf4646c40746ecaaad31bc2a33
TelOfLolth <28494487+TelOfLolth@users.noreply.github.com>
2018-09-30 10:35:42 -0400

    Update login.js

--- login.js
+++ login.js
@@ -57,8 +57,8 @@ function toggleSignIn() {
           }
           console.log(error);
         });
+        window.location.href = "index.html";
       }
-    window.location.href = "index.html";
     }
 
 
